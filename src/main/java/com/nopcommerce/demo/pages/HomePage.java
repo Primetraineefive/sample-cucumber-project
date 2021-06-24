53418d89c5eee49d3d1118f7c4496413c0f3eb54
Primetraineefive <primetraineefive@gmail.com>
2021-06-24 22:21:35 +0100

    sample-cucumber-project

---
+++ src/main/java/com/nopcommerce/demo/pages/HomePage.java
@@ -0,0 +1,34 @@
+package com.nopcommerce.demo.pages;
+
+import com.nopcommerce.demo.utility.Utility;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Reporter;
+
+/**
+ * Created by Jay
+ */
+public class HomePage extends Utility {
+
+    private static final Logger log = LogManager.getLogger(HomePage.class.getName());
+
+    @FindBy(linkText = "Log in")
+    WebElement loginLink;
+
+    @FindBy(xpath = "//a[@class='ico-register']")
+    WebElement registerLink;
+
+    public void clickOnLoginLink() {
+        Reporter.log("Clicking on login link : " + loginLink.toString() + "<br>");
+        clickOnElement(loginLink);
+        log.info("Clicking on login link : " + loginLink.toString());
+    }
+
+    public void clickOnRegisterLink() {
+        Reporter.log("Clicking on login link : " + registerLink.toString() + "<br>");
+        clickOnElement(registerLink);
+        log.info("Clicking on login link : " + registerLink.toString());
+    }
+}
---
+++ src/main/java/com/nopcommerce/demo/pages/LoginPage.java
@@ -0,0 +1,62 @@
+package com.nopcommerce.demo.pages;
+
+import com.nopcommerce.demo.utility.Utility;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Reporter;
+
+
+/**
+ * Created by Jay
+ */
+public class LoginPage extends Utility {
+
+    private static final Logger log = LogManager.getLogger(LoginPage.class.getName());
+
+    @FindBy(id = "Email")
+    WebElement emailField;
+
+    @FindBy(id = "Password")
+    WebElement passwordField;
+
+    @FindBy(xpath = "//input[@class='button-1 login-button']")
+    WebElement loginBtn;
+
+    @FindBy(xpath = "//div[@class='page-title']//h1")
+    WebElement welcomeText;
+
+
+    public void enterEmailId(String email) {
+        Reporter.log("Entering email address : " + email + " to email field : " + emailField.toString() + "<br>");
+        sendTextToElement(emailField, email);
+        log.info("Entering email address : " + email + " to email field : " + emailField.toString());
+    }
+
+    public void enterPassword(String password) {
+        Reporter.log("Entering password : " + password + " to password field : " + passwordField.toString() + "<br>");
+        sendTextToElement(passwordField, password);
+        log.info("Entering password : " + password + " to password field : " + passwordField.toString());
+    }
+
+    public void clickOnLoginButton() {
+        Reporter.log("Clicking on login button : " + loginBtn.toString() + "<br>");
+        clickOnElement(loginBtn);
+        log.info("Clicking on login button : " + loginBtn.toString());
+    }
+
+    public String getWelcomeText() {
+        Reporter.log("Getting text from : " + welcomeText.toString() + "<br>");
+        log.info("Getting text from : " + welcomeText.toString());
+        return getTextFromElement(welcomeText);
+    }
+
+    public void loginToApplication(String username, String password) {
+        enterEmailId(username);
+        enterPassword(password);
+        clickOnLoginButton();
+    }
+
+
+}
